dc68077ac05f38ded63e69eea10b4985d695e292
Arturo Larios <arallariosqu@ittepic.edu.mx>
2022-10-22 16:53:38 -0500

    add: copy json with format

--- src/app/core/components/json-viewer/json-viewer.component.ts
+++ src/app/core/components/json-viewer/json-viewer.component.ts
@@ -20,7 +20,7 @@ export class JsonViewerComponent {
   async copy() {
     try {
       this.isCopying = true;
-      await navigator.clipboard.writeText(JSON.stringify(this.fake.data));
+      await navigator.clipboard.writeText(JSON.stringify(this.fake.data, undefined, 2));
       this.toast.success('Success', 'JSON copy to clipboard');
     } catch (error) {
       this.toast.showError('Error', 'Something went wrong to copy json');
